160431cba75f23f04500ff3f3a2a979a05d14ae6
tenten0213 <takehito.0213@gmail.com>
2014-04-28 09:56:23 +0000

    add classify.py

---
+++ classify.py
@@ -0,0 +1,10 @@
+import pickle
+import constants
+import sys
+
+if __name__ == '__main__':
+    with open(constants.NB_PKL_FILENAME, 'rb') as f:
+        nb = pickle.load(f)
+    for query in sys.stdin:
+        result = nb.classify(query)
+        print('推測されたカテゴリは %s です' % result)
