135f84620cc02ff5c6d602028f801dba39a1fe90
tenten0213 <takehito.0213@gmail.com>
2014-05-08 07:08:24 +0000

    add tfidf

--- constants.py
+++ constants.py
@@ -1,4 +1,9 @@
 FETCHED_PAGES_DIR_NAME = 'fetched_pages'
 QUERY = '骨折'
+QUERIES = '胃もたれ 虫歯 花粉症対策 鬱 機械 骨折 肩こり 書類'.split(' ')
 NUM_OF_FETCHED_PAGES = 50
 NB_PKL_FILENAME = 'naive_bayes_classifier.pkl'
+DOC_NUM = 0
+MIN_TFIDF = 0.1
+TFIDF_RESULT_PKL_FILENAME = 'tfidf_result.pkl'
+TFIDF_VECTORIZERPKL_FILENAME = 'tfidf_vectorizer.pkl'
